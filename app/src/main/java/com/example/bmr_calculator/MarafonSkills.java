30b9dc48699f9bb678e9405accfa2b35e319c7db
aremneva <remnevaa0@gmail.com>
2021-04-04 18:50:43 +0300

    added new activity 'MarafonSkills'

---
+++ app/src/main/java/com/example/bmr_calculator/MarafonSkills.java
@@ -0,0 +1,14 @@
+package com.example.bmr_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MarafonSkills extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marafon_skills);
+    }
+}
